4cf1b1ba4ef02b73eb92276040068ff7ad7624ef
kroShot86 <kroShot86@mail.ru>
2025-10-17 20:41:35 +1000

    функция сохранения

--- To_Do_List/Window1.xaml.cs
+++ To_Do_List/Window1.xaml.cs
@@ -1,47 +1,102 @@
-﻿using System;
+﻿using Newtonsoft.Json;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using static To_Do_List.Window2;
+using System.Windows.Media;
 
 namespace To_Do_List
 {
-    /// <summary>
-    /// Логика взаимодействия для Window1.xaml
-    /// </summary>
     public partial class Window1 : Window
     {
         public List<Quest> quests = new List<Quest>();
         public static Window2 window2;
+
         public Window1()
         {
             InitializeComponent();
+            LoadQuests();
+            this.Closing += Window_Closing;
         }
 
-        //авпвывыв
         public StackPanel StackPanel1 = new StackPanel();
         public StackPanel StackPanel2 = new StackPanel();
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
 
+
+        private void LoadQuests()
+        {
+            if (File.Exists("quests.json"))
+            {
+                var json = File.ReadAllText("quests.json");
+                quests = JsonConvert.DeserializeObject<List<Quest>>(json);
+
+                foreach (var quest in quests)
+                {
+                    AddQuestButton(quest);
+                }
+            }
+        }
+
+        public void AddQuestButton(Quest quest)
+        {
+            Button newButton = new Button
+            {
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                Background = null,
+                Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF37F7EA")),
+                FontSize = 24,
+            };
+            TextBlock textBlock = new TextBlock
+            {
+                Text = quest.Name,
+                TextWrapping = TextWrapping.Wrap,
+                TextAlignment = TextAlignment.Center
+            };
+
+            newButton.Content = textBlock;
+            newButton.Click += (s, args) => { Podrobnosti.Text = quest.Description; };
+            newButton.MouseDoubleClick += (s, args) =>
+            {
+                if (StackPanel1.Children.Contains(newButton))
+                {
+                    StackPanel1.Children.Remove(newButton);
+                    StackPanel2.Children.Add(newButton);
+                    quests.Remove(quest);
+                }
+                else
+                {
+                    StackPanel2.Children.Remove(newButton);
+                }
+            };
+
+            StackPanel1.Children.Add(newButton);
+            figny.Content = StackPanel1;
+            figny1.Content = StackPanel2;
+        }
+
+        private void SaveQuests()
+        {
+            var json = JsonConvert.SerializeObject(quests);
+            File.WriteAllText("quests.json", json);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            SaveQuests();
+        }
+
+        private void New_kw(object sender, RoutedEventArgs e)
+        {
             if (window2 == null || !window2.IsVisible)
             {
+
                 window2 = new Window2(this);
                 window2.Owner = this;
                 window2.Show();
             }
-            
-
         }
     }
 }
--- To_Do_List/Window2.xaml.cs
+++ To_Do_List/Window2.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using static To_Do_List.Window2;
 
+using Newtonsoft.Json;
+using System.IO;
+
 namespace To_Do_List
 {
     /// <summary>
@@ -33,8 +36,6 @@ namespace To_Do_List
             }
         }
 
-        
-
         public Window1 window1;
 
         public Window2(Window1 owner)
@@ -48,64 +49,40 @@ namespace To_Do_List
             
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Name_kw(object sender, RoutedEventArgs e)
         {
             NameKv.Visibility = Visibility.Visible;
             NameKv.Focus();
         }
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void Opis_kw(object sender, RoutedEventArgs e)
         {
             OpisKv.Visibility = Visibility.Visible;
             OpisKv.Focus();
         }
 
-        private void Button_Click_2(object sender, RoutedEventArgs e)
+        private void Otmena(object sender, RoutedEventArgs e)
         {
             window1.Activate();
             this.Close();
         }
 
-        private void Button_Click_3(object sender, RoutedEventArgs e)
+        private void Save(object sender, RoutedEventArgs e)
         {
             if (NameKv.Text != "")
             {
                 Quest newQuest = new Quest(NameKv.Text, OpisKv.Text);
                 window1.quests.Add(newQuest);
-
-                Button newButton = new Button
-                {
-                    HorizontalAlignment = HorizontalAlignment.Center, // Выравнивание по горизонтали
-                    VerticalAlignment = VerticalAlignment.Center,
-                    Background = null,
-                    Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF37F7EA")),
-                    FontSize = 24,
-                };
-                TextBlock textBlock = new TextBlock
-                {
-                    Text = NameKv.Text,
-                    TextWrapping = TextWrapping.Wrap, // Включаем перенос текста
-                    TextAlignment = TextAlignment.Center // Выравнивание текста по центру
-                };
-
-                newButton.Content = textBlock;
-                newButton.Click += (s, args) => { window1.Podrobnosti.Text = newQuest.Description; };
-                newButton.MouseDoubleClick += (s, args) =>
-                {
-                    if (window1.StackPanel1.Children.Contains(newButton))
-                    {
-                        window1.StackPanel1.Children.Remove(newButton);
-                        window1.StackPanel2.Children.Add(newButton);
-                    }
-                    
-                };
-                window1.StackPanel1.Children.Add(newButton);
-                window1.figny.Content = window1.StackPanel1;
-                window1.figny1.Content = window1.StackPanel2;
+                window1.AddQuestButton(newQuest);
 
                 window1.Activate();
                 this.Close();
-                    
+
             }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
